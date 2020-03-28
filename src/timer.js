b49e3cb5ca750eaf778ddf116969693f68279a4c
jgbijlsma <jbijlsma.p@gmail.com>
2020-03-28 17:05:09 +0100

    moved project to this create-electron-app

---
+++ src/timer.js
@@ -0,0 +1,116 @@
+const timeCurrentElement = document.getElementById("time-current");
+const timeTimerElement = document.getElementById("time-timer");
+const btnStart = document.getElementById("btn-start");
+const btnStop = document.getElementById("btn-stop");
+const btnPause = document.getElementById("btn-pause");
+const btnBreak5 = document.getElementById("btn-break-5");
+const btnBreak10 = document.getElementById("btn-break-10");
+const btnBreak30 = document.getElementById("btn-break-30");
+
+// set up the display state of the buttons
+
+btnStop.classList.add("hidden");
+btnPause.classList.add("hidden");
+
+// sets the current time display
+const setTimeCurrent = () => {
+  timeCurrent = new Date();
+  timeCurrentElement.innerText = timeCurrent.toLocaleTimeString("UK");
+};
+
+// sets the remaining time display
+const setTimeTimer = () => {
+  timeRemaining = new Date(targetTime - timeCurrent);
+  timeRemaining.setHours(0);
+  timeTimerElement.innerText = timeRemaining.toLocaleTimeString("UK");
+
+  // theck if the timer is at 0
+  if (timeRemaining.getMinutes() === 0 && timeRemaining.getSeconds() === 0) {
+    btnStart.classList.remove("hidden");
+    btnStop.classList.add("hidden");
+    btnPause.classList.add("hidden");
+    btnPause.innerText = "Pause";
+    onTimerEnd();
+    clearInterval(timeTimerInterval);
+  }
+};
+
+// initialize the timer countdown
+const startTimer = duration => {
+  targetTime = new Date();
+  targetTime.setMinutes(targetTime.getMinutes() + duration);
+  timeTimerInterval = setInterval(setTimeTimer, 1000);
+  btnStart.classList.add("hidden");
+  btnStop.classList.remove("hidden");
+  btnPause.classList.remove("hidden");
+};
+
+// stop the timer and reset back to 0
+const stopTimer = () => {
+  timeTimerElement.innerText = "00:00:00";
+  clearInterval(timeTimerInterval);
+  timeTimerInterval = undefined;
+  btnStart.classList.remove("hidden");
+  btnStop.classList.add("hidden");
+  btnPause.classList.add("hidden");
+  btnPause.innerText = "Pause";
+};
+
+// pause the timer, or restart if currently paused
+const pauseOrRestartTimer = () => {
+  if (timeTimerInterval) {
+    clearInterval(timeTimerInterval);
+    timeTimerInterval = undefined;
+    timeAtPause = new Date();
+    btnPause.innerText = "Resume";
+  } else {
+    const curTime = new Date();
+    const timeBetween = curTime - timeAtPause;
+    targetTime.setMilliseconds(targetTime.getMilliseconds() + timeBetween);
+    timeTimerInterval = setInterval(setTimeTimer, 1000);
+    btnPause.innerText = "Pause";
+  }
+};
+
+// play a sound
+const onTimerEnd = () => {
+  const sound = new Audio("./alarm.mp3");
+  sound.play();
+};
+
+// event listeners
+
+btnStart.addEventListener("click", () => {
+  startTimer(25);
+});
+btnStop.addEventListener("click", stopTimer);
+btnPause.addEventListener("click", pauseOrRestartTimer);
+btnBreak5.addEventListener("click", () => {
+  stopTimer();
+  startTimer(5);
+});
+btnBreak10.addEventListener("click", () => {
+  stopTimer();
+  startTimer(10);
+});
+btnBreak30.addEventListener("click", () => {
+  stopTimer();
+  startTimer(30);
+});
+
+// setup the current time
+let timeCurrent;
+setTimeCurrent();
+const timeCurrentInterval = setInterval(setTimeCurrent, 1000);
+
+// set up the timer time
+let timeTimerInterval;
+let targetTime;
+let timeRemaining;
+let timeTimer = new Date();
+timeTimer.setHours(0);
+timeTimer.setMinutes(0);
+timeTimer.setSeconds(0);
+
+// keep track of the passed time when paused
+let timeAtPause;
---
+++ todo.md
@@ -0,0 +1,15 @@
+# must have
+
+- [x] display the current time
+- [x] start button (25 minutes)
+- [x] display the timer time
+- [x] play a sound when the time is up
+- [x] stop (reset) button
+- [x] pause / continue button
+- [x] rename and hide buttons conditionally
+- [x] button for 5, 10, 30, minutes break
+- [x] add style
+
+# nice to have
+
+- [ ] show the time when the timer will ring
